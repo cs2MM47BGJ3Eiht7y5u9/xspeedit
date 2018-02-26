8fa85141753d2a3eefde2ccdf4306cf99bf97e23
Mael Auzias <cs2MM47BGJ3Eiht7y5u9-github@mael.auzias.net>
2018-02-26 23:38:34 +0100

    Constants are `final`

--- src/main/org/java/util/Constant.java
+++ src/main/org/java/util/Constant.java
@@ -1,11 +1,11 @@
 package org.java.util;
 
 public interface Constant {
-    static int MINIMAL_WEIGHT = 1;
-    static int PRODUCT_MAXIMAL_WEIGHT = 9;
-    static int BOX_MAXIMAL_WEIGHT = 10;
-    static String EXCEPTION_MESSAGE_BASE = "Weight is too ";
-    static String EXCEPTION_MESSAGE_UNIT = " unit).";
-    static String EXCEPTION_MESSAGE_HEAVY = EXCEPTION_MESSAGE_BASE + "heavy (";
-    static String EXCEPTION_MESSAGE_LIGHT = EXCEPTION_MESSAGE_BASE + "light (";
+    static final int MINIMAL_WEIGHT = 1;
+    static final int PRODUCT_MAXIMAL_WEIGHT = 9;
+    static final int BOX_MAXIMAL_WEIGHT = 10;
+    static final String EXCEPTION_MESSAGE_BASE = "Weight is too ";
+    static final String EXCEPTION_MESSAGE_UNIT = " unit).";
+    static final String EXCEPTION_MESSAGE_HEAVY = EXCEPTION_MESSAGE_BASE + "heavy (";
+    static final String EXCEPTION_MESSAGE_LIGHT = EXCEPTION_MESSAGE_BASE + "light (";
 }
